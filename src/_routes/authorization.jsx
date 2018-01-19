ef45e79976a64be46722776d9ffd01abd76cfeed
Faizil <mfaizil1993@gmail.com>
2018-01-19 10:59:43 +0530

    routes added

---
+++ src/_routes/authorization.jsx
@@ -0,0 +1,28 @@
+
+
+const Authorization = (allowedRoles) =>
+(WrappedComponent) =>
+  return class WithAuthorization extends React.Component {
+    constructor(props) {
+      super(props)
+
+      console.log("INSIDE AUTHORIZATION CONSTRUCTOR")
+
+        user: {
+          name: 'vcarl',
+          role: 'admin'
+        }
+      }
+    }
+    render() {
+      const { role } = this.state.user
+      if (allowedRoles.includes(role)) {
+        return <WrappedComponent {...this.props} />
+      } else {
+        return <h1>No page for you!</h1>
+      }
+    }
+  }
+
+
+  export { Authorization };
